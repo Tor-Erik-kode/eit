8b85a0d174c751574fa2b453c10a31b095e5f0d3
Synnøve Halle <synnovsh@users.noreply.github.com>
2020-02-07 13:43:04 +0100

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5e3d5b54d96201b0b1e332c6',
+                  title: 'Sanity Studio',
+                  name: 'eit-studio',
+                  apiId: '542d8bde-7154-4bab-94e4-3d6ff2b38ff6'
+                },
+                {
+                  buildHookId: '5e3d5b54b73a1cc874037889',
+                  title: 'Portfolio Website',
+                  name: 'eit-web',
+                  apiId: '9ee90eb9-fab5-4b64-bbe2-05a437b38197'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/synnovsh/eit',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'https://eit-web.netlify.com',
+            category: 'apps'
+          }
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'j9a8ka6r',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
